edf41a2d6f05f5e273a9fc795d74572770eefde8
lucasdrw <moraesluk01@gmail.com>
2017-05-21 21:50:10 -0300

    criaçao da tela suporte, finalizada

--- app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/Suporte.java
+++ app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/Suporte.java
@@ -3,12 +3,17 @@ package com.cursoandroid.centralpet.centralpet.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.cursoandroid.centralpet.centralpet.R;
 
 public class Suporte extends AppCompatActivity {
 
     private Toolbar toolbar_suporte;
+
+    private EditText cxsuporte;
+    private Button btfeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,9 @@ public class Suporte extends AppCompatActivity {
         toolbar_suporte = (Toolbar) findViewById(R.id.toolbar_suporte);
         toolbar_suporte.setTitle("Suporte");
         setSupportActionBar(toolbar_suporte);
+
+        cxsuporte = (EditText) findViewById(R.id.cxsuporte);
+        btfeedback = (Button) findViewById(R.id.btfeedback);
 
     }
 }
