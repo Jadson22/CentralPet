0dc5562669b36a396fcf67a92e2d5dca564ca1fb
Jadson22 <jadsonufc@gmail.com>
2017-05-30 22:48:18 -0300

    adicionado o google maps

--- app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/MapaClinicaActivity.java
+++ app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/MapaClinicaActivity.java
@@ -41,7 +41,9 @@ public class MapaClinicaActivity extends FragmentActivity implements OnMapReadyC
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.addMarker(new MarkerOptions()
+                .position(sydney)
+                .title("central  pet"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
