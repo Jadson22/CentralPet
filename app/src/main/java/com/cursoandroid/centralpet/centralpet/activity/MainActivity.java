b6119302553bd09486362cd6fdd022f002bdcdd3
lucasdrw <moraesluk01@gmail.com>
2017-07-13 12:58:15 -0300

    agora ta o login com o google e vai ficar assim

--- app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/MainActivity.java
@@ -1,8 +1,10 @@
 package com.cursoandroid.centralpet.centralpet.activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 import com.cursoandroid.centralpet.centralpet.R;
@@ -12,17 +14,47 @@ import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     //private LoginButton loginButton;
     //private CallbackManager callbackManager;
+    private GoogleApiClient googleApiClient;
+    private SignInButton signInButton;
+    public static final int SIGN_IN_CODE = 777;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        signInButton = (SignInButton) findViewById(R.id.signInButton);
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent, SIGN_IN_CODE);
+            }
+        });
+
+        signInButton.setSize(SignInButton.SIZE_WIDE);
+        signInButton.setColorScheme(SignInButton.COLOR_DARK);
 
         //callbackManager = CallbackManager.Factory.create();
         //loginButton = (LoginButton) findViewById(R.id.login_button);
@@ -46,15 +78,35 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    //private void goMainScreen() {
-    //    Intent intent = new Intent(this, tela_menu.class);
-    //    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-    //    startActivity(intent);
-    //}
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         //callbackManager.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN_CODE){
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handleSignInResult(result);
+        }
+
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        if(result.isSuccess()){
+            goMainScreen();
+        }else{
+            Toast.makeText(this, "Não foi possivel iniciar", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    private void goMainScreen() {
+        Intent intent = new Intent(this, tela_menu.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/tela_menu.java
+++ app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/tela_menu.java
@@ -2,6 +2,7 @@ package com.cursoandroid.centralpet.centralpet.activity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -11,16 +12,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.cursoandroid.centralpet.centralpet.R;
 import com.facebook.AccessToken;
 import com.facebook.login.LoginManager;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class tela_menu extends AppCompatActivity{
+public class tela_menu extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
     private AdView adView;
 
@@ -33,6 +44,8 @@ public class tela_menu extends AppCompatActivity{
     private ImageView suporte;
     private ImageView meusPets;
     private ImageView clinicas;
+
+    private GoogleApiClient googleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,11 +65,18 @@ public class tela_menu extends AppCompatActivity{
         toolbar_menu.setTitle("Central Pet");
         setSupportActionBar(toolbar_menu);
 
-
-
         //if(AccessToken.getCurrentAccessToken() == null){
         //    goLoginScreen();
         //}
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
 
 
 
@@ -121,21 +141,44 @@ public class tela_menu extends AppCompatActivity{
         });
 
 
-
-
         //Anúncios
         adView = (AdView) findViewById(R.id.adView);
         AdRequest adRequest = new AdRequest.Builder().build();
         adView.loadAd(adRequest);
-
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if(opr.isDone()){
+            GoogleSignInResult result = opr.get();
+            handleSignInResult(result);
+        }else{
+            opr.setResultCallback(new ResultCallback<GoogleSignInResult>() {
+                @Override
+                public void onResult(@NonNull GoogleSignInResult googleSignInResult) {
+                    handleSignInResult(googleSignInResult);
+                }
+            });
+        }
+    }
 
-    //private void goLoginScreen(){
-    //    Intent intent = new Intent(this, MainActivity.class);
-    //   intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-    //   startActivity(intent);
-    //}
+    private void handleSignInResult(GoogleSignInResult result) {
+        if(result.isSuccess()){
+            GoogleSignInAccount account = result.getSignInAccount();
+
+            //nome.setText(account.getDisplayName());
+        }else{
+            goLoginScreen();
+        }
+    }
+
+    private void goLoginScreen(){
+        Intent intent = new Intent(this, MainActivity.class);
+       intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+       startActivity(intent);
+    }
 
 
     //toolbar
@@ -158,9 +201,23 @@ public class tela_menu extends AppCompatActivity{
     }
         public void deslogar_usuario(){
         //LoginManager.getInstance().logOut();
-        //goLoginScreen();
+        goLoginScreen();
+            Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+                @Override
+                public void onResult(@NonNull Status status) {
+                    if(status.isSuccess()){
+                        goLoginScreen();
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Não foi possivel encerrar seção", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
 
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
 }
 
 
