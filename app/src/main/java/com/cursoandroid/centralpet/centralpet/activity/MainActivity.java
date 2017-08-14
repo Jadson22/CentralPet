5301c7b5c0e3fbfcce45035abf0066fcc33c291c
lucasdrw <moraesluk01@gmail.com>
2017-08-14 20:55:33 -0300

    esta ok

--- app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/MainActivity.java
+++ app/src/main/java/com/cursoandroid/centralpet/centralpet/activity/MainActivity.java
@@ -9,12 +9,6 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.cursoandroid.centralpet.centralpet.R;
-import com.facebook.CallbackManager;
-import com.facebook.FacebookCallback;
-import com.facebook.FacebookException;
-import com.facebook.login.LoginManager;
-import com.facebook.login.LoginResult;
-import com.facebook.login.widget.LoginButton;
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
